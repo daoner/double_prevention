04585116d7d141fdf355939593f5d0bb6b009800
cuijiahao <15927252954@163.com>
2019-10-13 15:16:51 +0800

    risk table item

--- src/pages/riskMoudle/dangerManage/index.jsx
+++ src/pages/riskMoudle/dangerManage/index.jsx
@@ -1,6 +1,85 @@
 import React, { Component } from 'react';
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Table } from 'antd';
 import './style.css';
+
+const dataSource = [
+    { 
+        key: '0123',
+        riskplace: '风险的位置',
+        name: '易燃物爆炸',
+        type: '易发生的事故类型',
+        L: '可能性',
+        E: '频繁程度',
+        C: '事故后果',
+        D: '风险值',
+        degree: '危险源源等级',
+        measure: '措施1 2 3 ',
+        level: '管控层级',
+        depId: '责任单位',
+        userId: '责任人id'
+        
+    },
+    { 
+        key: '0123',
+        riskplace: '风险的位置',
+        name: '易燃物爆炸',
+        type: '易发生的事故类型',
+        L: '可能性',
+        E: '频繁程度',
+        C: '事故后果',
+        D: '风险值',
+        degree: '危险源源等级',
+        measure: '措施1 2 3 ',
+        level: '管控层级',
+        depId: '责任单位',
+        userId: '责任人id'
+        
+    },
+    { 
+        key: '0123',
+        riskplace: '风险的位置',
+        name: '易燃物爆炸',
+        type: '易发生的事故类型',
+        L: '可能性',
+        E: '频繁程度',
+        C: '事故后果',
+        D: '风险值',
+        degree: '危险源源等级',
+        measure: '措施1 2 3 ',
+        level: '管控层级',
+        depId: '责任单位',
+        userId: '责任人id'
+        
+    }
+];
+
+const columns = [
+    {
+        title: 'ID',
+        dataIndex: 'key',
+        key: 'id'
+    },
+    {
+        title: '危险源名称',
+        dataIndex: 'name',
+        key: 'name'
+    },
+    {
+        title: '易发生事故类型',
+        dataIndex: 'type',
+        key: 'type'
+    }, 
+    {
+        title: '风险值（L*E*C)',
+        dataIndex: 'D',
+        key: 'D'
+    },
+    {
+        title: '危险源等级',
+        dataIndex: 'degree',
+        key: 'degree'
+    }   
+]
 
 class DangerManage extends Component {
     render() {
@@ -12,8 +91,8 @@ class DangerManage extends Component {
                     <Breadcrumb.Item>危险源管理</Breadcrumb.Item>
                 </Breadcrumb>
                 {/* 内容区域 */}
-                <div>
-                危险源管理
+                <div className="contentWrap">
+                    <Table dataSource={dataSource} columns={columns} />
                 </div>
             </div>
         )
--- src/pages/riskMoudle/riskManage/index.jsx
+++ src/pages/riskMoudle/riskManage/index.jsx
@@ -1,7 +1,58 @@
 import React, { Component } from 'react';
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Table } from 'antd';
 import './style.css';
+const dataSource = [
+    {
+        key:"1",
+        name: '风险点xxxx',
+        place: '风险位置',
+        level: '3',
+        telephone: '5434345'
+    },
+    {
+        key:"2",
+        name: '风险点xxx',
+        place: '风险位置',
+        level: '2',
+        telephone: '12356786'
+    },
+    {
+        key:"3",
+        name: '风险点xxx',
+        place: '风险位置',
+        level: '2',
+        telephone: '123456879'
+       },
+];
 
+const columns = [
+    {
+        title: 'ID',
+        dataIndex: 'key',
+        key: 'id'
+     },
+    {
+       title: '风险点',
+       dataIndex: 'name',
+       key: 'name'
+    },
+    {
+        title: '风险点位置',
+        dataIndex: 'place',
+        key: 'place'
+    },
+    {
+       title: '风险点等级',
+       dataIndex: 'level',
+       key: 'level'
+    },
+    {
+        title: '应急电话',
+        dataIndex: 'telephone',
+        key: 'telephone'
+     }
+  ];
+  
 class RiskManage extends Component {
     render() {
         return (
@@ -12,8 +63,8 @@ class RiskManage extends Component {
                     <Breadcrumb.Item>风险管理</Breadcrumb.Item>
                 </Breadcrumb>
                 {/* 内容区域 */}
-                <div>
-                风险管理
+                <div className="contentWrap">
+                    <Table dataSource={dataSource} columns={columns} />
                 </div>
             </div>
         )
