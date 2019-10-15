b957aeadbe30c811f80fd39b2a4a41fd3ec0715e
cuijiahao <15927252954@163.com>
2019-10-15 17:52:16 +0800

    add table to fistIndicatorManage

--- src/pages/securityMoudle/firstIndicatorManage/index.jsx
+++ src/pages/securityMoudle/firstIndicatorManage/index.jsx
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
+import { Link }from 'react-router-dom';
+
+import { Table, Button, Icon, Tag, Divider, Input } from 'antd';
+const Search = Input.Search;
 
 class FirstIndicatorManage extends Component {
     render() {
+        //table数据
+        const dataSource = [
+            { id: 1, project: '作业行为', addDate: '2012-5-23' }
+        ];
+
+        //table 列属性
+        const ccc = [
+            { title: 'ID', dataIndex: 'id', key: 'id' },
+            { title: '项目', dataIndex: 'project', key: 'project' },
+            { title: '添加时间', dataIndex: 'addDate', key: 'addDate' },
+            {
+                title: '操作',
+                key: 'action',
+                render: ()=>(<span>
+                    <Tag color="blue">详情</Tag>
+                    <Divider type="vertical"/>
+                    <Tag color="red">删除</Tag>
+                </span>)
+            }
+        ];
+
         return (
-            <div>first level indicator manager</div>
+            <div>
+                <div className="contentWrap">
+                    <div style={{width:'90%',margin:'20px auto',height:'80px'}}>
+                        <Search
+                            className="searchClass"
+                            placeholder="input search text"
+                            enterButton="Search"
+                            size="large"
+                            onSearch={value => console.log(value)}
+                        />
+                        <Link>
+                        <Button style={{float:'right',width:'79px',height:'40px',margin:'20px 0px'}}><Icon type="plus" />添加</Button>
+                        </Link>
+                    </div>
+                    <Table
+                        className="tableClass"
+                        bordered="true"
+                        columns={ccc} dataSource={dataSource} />
+                </div>
+            </div>
         )
     }
 }
