8b60e96df420ea461c447021f145346e2fe28fd6
cuijiahao <15927252954@163.com>
2019-10-18 14:56:59 +0800

    design checktable preview page

--- src/pages/securityMoudle/checkTableInput/index.jsx
+++ src/pages/securityMoudle/checkTableInput/index.jsx
@@ -1,10 +1,114 @@
 import React, { Component } from 'react';
+import { Breadcrumb, Card, Icon } from 'antd';
+
+import './style.css';
+import imgURL from '../../../statics/images/checktable_preview.png';
 
 class CheckTableInput extends Component {
     render() {
         return (
-            <div>
-                检查表录入，显示列表，选择一项，进入填写提交页面
+            <div className="page">
+                {/* 导航路径 */}
+                <Breadcrumb className="path">
+                    <Breadcrumb.Item>安全检查与隐患管理</Breadcrumb.Item>
+                    <Breadcrumb.Item>检查表一览</Breadcrumb.Item>
+                </Breadcrumb>
+                <div className="contentWrap" >
+                    <ul className="card-list clearfix">
+                        <li>
+                            <Card 
+                                cover={ <img alt="name" src={imgURL} /> }
+                                actions={[
+                                    <span key='detail'><Icon type="info-circle" />详情</span>,
+                                    <span key='edit'><Icon type="edit"/>录入</span>,
+                                ]}
+                            >
+                                <Card.Meta
+                                    title="矿山日常制度检查表"
+                                />
+                            </Card>
+                        </li>
+                        <li>
+                            <Card 
+                                cover={ <img alt="name" src={imgURL} /> }
+                                actions={[
+                                    <span key='detail'><Icon type="info-circle" />详情</span>,
+                                    <span key='edit'><Icon type="edit"/>录入</span>,
+                                ]}
+                            >
+                                <Card.Meta
+                                    title="矿山日常制度检查表"
+                                />
+                            </Card>
+                        </li>
+                        <li>
+                            <Card 
+                                cover={ <img alt="name" src={imgURL} /> }
+                                actions={[
+                                    <span key='detail'><Icon type="info-circle" />详情</span>,
+                                    <span key='edit'><Icon type="edit"/>录入</span>,
+                                ]}
+                            >
+                                <Card.Meta
+                                    title="矿山日常制度检查表"
+                                />
+                            </Card>
+                        </li>
+                        <li>
+                            <Card 
+                                cover={ <img alt="name" src={imgURL} /> }
+                                actions={[
+                                    <span key='detail'><Icon type="info-circle" />详情</span>,
+                                    <span key='edit'><Icon type="edit"/>录入</span>,
+                                ]}
+                            >
+                                <Card.Meta
+                                    title="矿山日常制度检查表"
+                                />
+                            </Card>
+                        </li>
+                        <li>
+                            <Card 
+                                cover={ <img alt="name" src={imgURL} /> }
+                                actions={[
+                                    <span key='detail'><Icon type="info-circle" />详情</span>,
+                                    <span key='edit'><Icon type="edit"/>录入</span>,
+                                ]}
+                            >
+                                <Card.Meta
+                                    title="矿山日常制度检查表"
+                                />
+                            </Card>
+                        </li>
+                        <li>
+                            <Card 
+                                cover={ <img alt="name" src={imgURL} /> }
+                                actions={[
+                                    <span key='detail'><Icon type="info-circle" />详情</span>,
+                                    <span key='edit'><Icon type="edit"/>录入</span>,
+                                ]}
+                            >
+                                <Card.Meta
+                                    title="矿山日常制度检查表"
+                                />
+                            </Card>
+                        </li>
+                        <li>
+                            <Card 
+                                cover={ <img alt="name" src={imgURL} /> }
+                                actions={[
+                                    <span key='detail'><Icon type="info-circle" />详情</span>,
+                                    <span key='edit'><Icon type="edit"/>录入</span>,
+                                ]}
+                            >
+                                <Card.Meta
+                                    title="矿山日常制度检查表"
+                                />
+                            </Card>
+                        </li> 
+                    </ul>
+                </div>
+                
             </div>
         )
     }
